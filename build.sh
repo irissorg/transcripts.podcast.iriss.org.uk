e81228b650100f3c1c38d68a107738ac64733d74
Lesley Duff <lesley.duff@iriss.org.uk>
2019-08-15 11:29:05 +0100

    Add choose publish dates in future

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ echo -e "\033[0;32mBuilding site in docs folder...\033[0m"
 
 rm -rf docs
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
+# Build the project. Ask whether to publish future dates
+while true; do
+    read -p "Publish future dates? (y/n)" yn
+    case $yn in
+        [Yy]* ) hugo -F;  break;;
+        [Nn]* ) hugo; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,15 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 rm -rf docs
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Build the project. Ask whether to publish future dates
+while true; do
+    read -p "Publish future dates? (y/n)" yn
+    case $yn in
+        [Yy]* ) hugo -F;  break;;
+        [Nn]* ) hugo; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 # Go To Public folder
 cd docs
